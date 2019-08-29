3daade9b22633b585adb47e760baa7f40cae1dc6
Indu Ramesh <induramesh@gmail.com>
2019-08-29 20:42:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,18 @@
 class Song
   
   @@count = 0
+  @@genres = []
+  @@artists=[]
   
   def initialize(name, artist, genre)  
     # Instance variables  
     @name = name  
     @artist = artist
     @genre = genre
+    
     @@count += 1
+    @@genres.push(genre)
+    @@artists.push(artist)
   end  
   
   #accessors
@@ -18,4 +23,8 @@ class Song
   def count  
    @@count 
   end  
+  
+  def genres
+    @@genres
+  end 
 end 
