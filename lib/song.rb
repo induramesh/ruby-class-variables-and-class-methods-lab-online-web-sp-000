15d27fd2acb5f5ed54883bd99345bcaf5f238b13
Indu Ramesh <induramesh@gmail.com>
2019-08-29 20:36:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,15 @@
 class Song
+    def initialize(name, artist, genre)  
+    # Instance variables  
+    @name = name  
+    @artist = artist
+    @genre = genre
+  end  
+  
+  attr_accessor(new_name) 
+  
+  
+  def display  
+    puts "I am of #{@breed} breed and my name is #{@name}"  
+  end  
 end 
