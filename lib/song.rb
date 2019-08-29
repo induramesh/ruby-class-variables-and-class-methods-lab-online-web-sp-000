5f448d4c1bfbceb0a1726548e9936181ffadc859
Indu Ramesh <induramesh@gmail.com>
2019-08-29 20:58:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
     @@genres.each_with_object(Hash.new(0)) { |genre,counts| counts[genre] += 1 }
   end 
   
-  def self.genre_count
+  def self.artist_count
     @@artists.each_with_object(Hash.new(0)) { |artist,counts| counts[artist] += 1 }
   end 
   
